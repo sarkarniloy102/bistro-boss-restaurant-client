b96cded9633b1644bf0f9d64c2ee943a7056d535
sarkar niloy <sarkarniloy102@gmail.com>
2023-11-21 20:22:26 +0600

    helmet for changing title according to path

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,10 +3,12 @@ import Category from "../Category/Category";
 import Featured from "../Featured/Featured";
 import PopularMenu from "../PopularMenu/PopularMenu";
 import Testimonials from "../Testomonials/Testomonials";
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
     return (
         <div>
+          <Helmet><title>Bristo Boss | Home</title></Helmet>
           <Banner></Banner>
           <Category></Category>
           <PopularMenu></PopularMenu>
---
+++ src/Pages/Menu/Menu/Menu.jsx
@@ -0,0 +1,12 @@
+import { Helmet } from 'react-helmet-async';
+const Menu = () => {
+    return (
+        <div>
+            <Helmet><title>Bristo Boss | Menu</title></Helmet>
+
+            <h3>menu</h3>
+        </div>
+    );
+};
+
+export default Menu;
