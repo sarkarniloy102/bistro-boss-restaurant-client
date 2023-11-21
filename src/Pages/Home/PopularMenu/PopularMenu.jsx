6581bda6f516fb8e7065c790bf5170a47c56b781
sarkar niloy <sarkarniloy102@gmail.com>
2023-11-21 15:51:30 +0600

    Popular Menu in home section

---
+++ src/Pages/Home/PopularMenu/PopularMenu.jsx
@@ -0,0 +1,37 @@
+import { useEffect, useState } from "react";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+
+const PopularMenu = () => {
+    const [menu, setMenu] = useState([]);
+    useEffect(() => {
+        fetch('menu.json')
+            .then(res => res.json())
+            .then(data => {
+                const popularItems = data.filter(item => item.category === 'popular');
+                setMenu(popularItems)
+
+            })
+    }, [])
+    return (
+        <section className="mb-10">
+            <SectionTitle heading={"FROM OUR MENU"} subheading={"---Check it out---"}></SectionTitle>
+            <div className="grid md:grid-cols-2 gap-10">
+                {
+                   
+                    menu.map(item => <MenuItem
+                        key={item._id}
+                        item={item}
+                    ></MenuItem>)
+                }
+            </div>
+            <div className="flex justify-center">
+            <button className="btn btn-outline border-0 border-b-4 mt-4 ">View Full Menu</button>
+            </div>
+            
+        </section>
+    );
+};
+
+export default PopularMenu;
